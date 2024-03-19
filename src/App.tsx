97b82b0b0cb5e40bc112d9b73b30148f19c31b38
Stanley <stanleybrenner@gmail.com>
2024-03-18 21:17:16 -0300

    footer and adjusts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "@progress/kendo-theme-default/dist/all.css";
 import Navbar from "./components/Navbar";
 import GridGifs from "./components/Gridgifs";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <GridGifs />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,31 @@
+import "./index.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/staanb"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/stanley-brenner-front-end/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+      </ul>
+      <p>&copy; Stanley Brenner. Todos os direitos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
